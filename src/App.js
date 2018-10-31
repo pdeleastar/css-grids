27ca7bb50b90fb6d3cbdf93319d0e80aa248e4a7
Pierre de Leastar <pdeleastar@gmailcom>
2018-10-31 17:39:17 +0100

    feat: responsive grid added for all device widths

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import { grid, header, chart, news, keystats, overview, footer, Item } from "./grid";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className={grid}>
+        <Item className={header}>Search</Item>
+        <Item className={chart}>Chart</Item>
+        <Item className={news}>News</Item>
+        <Item className={keystats}>Keystats</Item>
+        <Item className={overview}>Overview</Item>
+        <Item className={footer}>Footer</Item>
       </div>
     );
   }
---
+++ src/grid.js
@@ -0,0 +1,76 @@
+import { css } from "react-emotion";
+import styled from "react-emotion";
+
+const fullScreen = 1280
+const tablet = 1280
+const mobile = 768
+
+const grid = css`
+  display: grid;
+  margin: 1.5vh;
+  grid-gap: 0.5vh;
+  grid-template-rows: 15vh 40vh 30vh 10vh;
+
+  @media (min-width: ${fullScreen}px) {
+    grid-template-areas:
+    "h h h h h h"
+    "c c c c n n"
+    "k k k k o o"
+    "f f f f f f";
+  };
+  @media (max-width: ${tablet}px) {
+    grid-template-areas:
+    "h h h h h h"
+    "c c c c c c"
+    "k k o o n n"
+    "f f f f f f";
+  };
+  @media (max-width: ${mobile}px) {
+    grid-template-areas:
+    "h h h h h h"
+    "c c c c c c"
+    "k k k k k k"
+    "n n n o o o"
+    "f f f f f f";
+  };
+`;
+
+const header = css`
+  grid-area: h;
+`;
+
+const chart = css`
+  grid-area: c;
+`;
+
+const news = css`
+  grid-area: n;
+`;
+
+const keystats = css`
+  grid-area: k;
+  
+`;
+
+const overview = css`
+  grid-area: o;
+`;
+
+const footer = css`
+  grid-area: f;
+`;
+
+
+
+const Item = styled("div")`
+  justify-content: center;
+  align-items: center;
+  font-size: 2em;
+  color: black;
+  background-color: #dededf;
+  border: 3px solid #f08c00;
+  border-radius: 5px;
+  padding: 3px;
+`;
+
+export { grid, header, chart, news, keystats, overview, footer, Item };
